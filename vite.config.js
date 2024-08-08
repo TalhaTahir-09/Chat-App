a1cca043d0036622ee2be9fe0bc4ea54226e0851
TalhaTahir-09 <hamzatalhabwp4@gmail.com>
2024-08-08 02:34:50 -0700

    Made changes for deployment 6

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'firebase/app': path.resolve(__dirname, 'node_modules/firebase/app'),
+      'firebase/auth': path.resolve(__dirname, 'node_modules/firebase/auth'),
+      'firebase/firestore': path.resolve(__dirname, 'node_modules/firebase/firestore'),
+    },
+  },
 });
